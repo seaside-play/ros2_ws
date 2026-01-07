79da87b5981982633816a67160df52eb5ce4e16c
chris <namtaerg@gmail.com>
2026-01-07 16:56:17 +0800

    完成两轮差速控制器控制机器人

--- src/robot_description/launch/gazebo_sim.launch.py
+++ src/robot_description/launch/gazebo_sim.launch.py
@@ -46,6 +46,18 @@ def generate_launch_description():
         output='screen'
     )
 
+    # 加载并激活 fishbot_effort_controller 控制器
+    load_fishbot_effort_controller = launch.actions.ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'fishbot_effort_controller'],
+        # cmd='ros2 control load_controller --set-state active fishbot_effort_controller'.split(' '), 
+        output='screen'
+    )
+
+    load_fishbot_diff_drive_controller = launch.actions.ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active','fishbot_diff_drive_controller'], 
+        output='screen'
+    )
+    
     return launch.LaunchDescription([
         action_declare_arg_model_path,
         robot_state_publisher_node,
@@ -56,6 +68,12 @@ def generate_launch_description():
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=spawn_entity_node,
                 on_exit=[load_joint_state_controller],
+            )
+        ),
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_fishbot_diff_drive_controller]
             )
         )
     ])
