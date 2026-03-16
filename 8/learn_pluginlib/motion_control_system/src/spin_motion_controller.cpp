2642513b45e72a23d672e2535a842468de978bab
chris <namtaerg@gmail.com>
2026-03-16 17:01:10 +0800

    完成ROS2插件创建并实现测试插件接口的调用功能

--- 8/learn_pluginlib/motion_control_system/src/spin_motion_controller.cpp
+++ 8/learn_pluginlib/motion_control_system/src/spin_motion_controller.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "motion_control_system/spin_motion_controller.hpp"
+#include "pluginlib/class_list_macros.hpp"
 
 namespace motion_control_system {
 
@@ -17,6 +18,5 @@ void SpinMotionController::stop() {
 
 // 导出插件，使其可以被ROS插件库加载，并且指定它实现了MotionController接口。
 // 第一个是要导出的类，第二个事导出的类的抽象基类
-PLUGINLIB_EXPORT_CLASS(motion_control_system::SpinMotionController, motion_control_system::MotionController)
-
 // 自此，编写好了插件的代码，接下来要编写插件的XML描述文件，告诉ROS插件库如何加载这个插件，以及它实现了哪个接口。
+PLUGINLIB_EXPORT_CLASS(motion_control_system::SpinMotionController, motion_control_system::MotionController)
